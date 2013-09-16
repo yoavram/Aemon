26b34b71582c3f88f9775ebd2dd6ce5ad16ec8db
Yoav Ram <yoavram@gmail.com>
2013-09-16 16:01:32 +0300

    basic bootstrap-jquery-mongo

---
+++ mockdb.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from flask.ext.pymongo import ObjectId
+DATE_FORMAT = "%d/%m/%Y"
+
+def datetime_from_string(string):
+	return datetime.strptime(string, DATE_FORMAT)
+
+class MockDb:
+	def __init__(self):
+		self.data = [
+			{'_id':ObjectId(),'name':u'uri', 'direction':'north', 'leaving':datetime_from_string('2/12/2012'), 'arriving':datetime_from_string('3/12/2012'), 'email':'uri@gmail.co', 'munged_email': 'uri [at] gmail [dot] co', 'source':'bariloche', 'destination':'al poson', 'secret':'secret1', 'comments':u"can't wait to get going, who's in?\nlet's get started"},
+			{'_id':ObjectId(),'name':u'מיכל', 'direction':'south', 'leaving':datetime_from_string('12/12/2012'), 'arriving':datetime_from_string('17/12/2012'), 'email':'yoavram@gmail.com', 'munged_email': 'yoavram [at] gmail [dot] com', 'source':'al poson', 'destination':'bariloche', 'secret':'secret2', 'comments':u'dying to take a piss'},
+			{'_id':ObjectId(),'name':u'uri', 'direction':'north', 'leaving':datetime_from_string('2/12/2012'), 'arriving':datetime_from_string('3/12/2012'), 'email':'uri@gmail.co', 'munged_email': 'uri [at] gmail [dot] co', 'source':'bariloche', 'destination':'al poson', 'secret':'secret1', 'comments':u"can't wait to get going, who's in?\nlet's get started"},
+			{'_id':ObjectId(),'name':u'מיכל', 'direction':'south', 'leaving':datetime_from_string('12/12/2012'), 'arriving':datetime_from_string('17/12/2012'), 'email':'yoavram@gmail.com', 'munged_email': 'yoavram [at] gmail [dot] com', 'source':'al poson', 'destination':'bariloche', 'secret':'secret2', 'comments':u'dying to take a piss'},
+			{'_id':ObjectId(),'name':u'uri', 'direction':'north', 'leaving':datetime_from_string('2/12/2012'), 'arriving':datetime_from_string('3/12/2012'), 'email':'uri@gmail.co', 'munged_email': 'uri [at] gmail [dot] co', 'source':'bariloche', 'destination':'al poson', 'secret':'secret1', 'comments':u"can't wait to get going, who's in?\nlet's get started"},
+			{'_id':ObjectId(),'name':u'מיכל', 'direction':'south', 'leaving':datetime_from_string('12/12/2012'), 'arriving':datetime_from_string('17/12/2012'), 'email':'yoavram@gmail.com', 'munged_email': 'yoavram [at] gmail [dot] com', 'source':'al poson', 'destination':'bariloche', 'secret':'secret2', 'comments':u'dying to take a piss'},
+			{'_id':ObjectId(),'name':u'uri', 'direction':'north', 'leaving':datetime_from_string('2/12/2012'), 'arriving':datetime_from_string('3/12/2012'), 'email':'uri@gmail.co', 'munged_email': 'uri [at] gmail [dot] co', 'source':'bariloche', 'destination':'al poson', 'secret':'secret1', 'comments':u"can't wait to get going, who's in?\nlet's get started"},
+			{'_id':ObjectId(),'name':u'מיכל', 'direction':'south', 'leaving':datetime_from_string('12/12/2012'), 'arriving':datetime_from_string('17/12/2012'), 'email':'yoavram@gmail.com', 'munged_email': 'yoavram [at] gmail [dot] com', 'source':'al poson', 'destination':'bariloche', 'secret':'secret2', 'comments':u'dying to take a piss'},
+			{'_id':ObjectId(),'name':u'uri', 'direction':'north', 'leaving':datetime_from_string('2/12/2012'), 'arriving':datetime_from_string('3/12/2012'), 'email':'uri@gmail.co', 'munged_email': 'uri [at] gmail [dot] co', 'source':'bariloche', 'destination':'al poson', 'secret':'secret1', 'comments':u"can't wait to get going, who's in?\nlet's get started"},
+			{'_id':ObjectId(),'name':u'מיכל', 'direction':'south', 'leaving':datetime_from_string('12/12/2012'), 'arriving':datetime_from_string('17/12/2012'), 'email':'yoavram@gmail.com', 'munged_email': 'yoavram [at] gmail [dot] com', 'source':'al poson', 'destination':'bariloche', 'secret':'secret2', 'comments':u'dying to take a piss'},
+			{'_id':ObjectId(),'name':u'uri', 'direction':'north', 'leaving':datetime_from_string('2/12/2012'), 'arriving':datetime_from_string('3/12/2012'), 'email':'uri@gmail.co', 'munged_email': 'uri [at] gmail [dot] co', 'source':'bariloche', 'destination':'al poson', 'secret':'secret1', 'comments':u"can't wait to get going, who's in?\nlet's get started"},
+			{'_id':ObjectId(),'name':u'מיכל', 'direction':'south', 'leaving':datetime_from_string('12/12/2012'), 'arriving':datetime_from_string('17/12/2012'), 'email':'yoavram@gmail.com', 'munged_email': 'yoavram [at] gmail [dot] com', 'source':'al poson', 'destination':'bariloche', 'secret':'secret2', 'comments':u'dying to take a piss'}
+		]
+
+
+	def insert(self, post):
+		_id = ObjectId()
+		post['_id'] = _id
+		self.data.append(post)
+		return _id
+
+
+	def find(self, spec=None, sort=None):
+		if spec:
+			data = []
+			for d in self.data:
+				match = True
+				for k,v in spec.items():
+					if d[k] != v:
+						match = False
+						break
+				if match:
+					data.append(d)
+			return data
+		else:
+			return self
+
+
+	def find_one(self, spec=None, sort=None):
+		data = self.find(spec, sort)
+		if len(data)>0:
+			return data[0]
+		else:
+			return None
+
+
+	def rewind(self):
+		return self
+
+
+	def __iter__(self):
+		return self.data.__iter__()
+
+
+	def count(self):
+		return len(self.data)
+
+
+	def remove(self, _id):
+		for x in self.data:
+			if x['_id'] == _id:
+				return self.data.remove(x)
--- web.py
+++ web.py
@@ -1,10 +1,70 @@
 import os
-from flask import Flask
+from flask import Flask, request, render_template, redirect, Response, url_for
+from flask.ext.pymongo import PyMongo, ObjectId
+import simplejson
+
+DATE_FORMAT = "%d/%m/%Y"
+SHORT_DATE_FORMAT = "%d/%m/%y"
+
+class MongoDocumentEncoder(simplejson.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+        elif isinstance(o, ObjectId):
+            return str(o)
+        return simplejson.JSONEncoder(self, o)
+
+def jsonify(*args, **kwargs):
+    return Response(simplejson.dumps(dict(*args, **kwargs), cls=MongoDocumentEncoder), mimetype='application/json')
+
+
+def db_name_from_uri(full_uri):
+	ind = full_uri[::-1].find('/')
+	return full_uri[-ind:]
+
+
+def datetime_from_string(string):
+	return datetime.strptime(string, DATE_FORMAT)
+
+
+def string_from_datetime(datetime_obj):
+	return datetime_obj.strftime(DATE_FORMAT)
+
+
+def short_string_from_datetime(datetime_obj):
+	return datetime_obj.strftime(SHORT_DATE_FORMAT)
+
+# add environment variables using 'heroku config:add VARIABLE_NAME=variable_name'
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+MONGO_URI = os.environ.get('MONGOLAB_URI')
+GOOGLE_ANALYTICS = os.environ.get('GOOGLE_ANALYTICS', '')
 
 app = Flask(__name__)
+app.config.from_object(__name__)  
+
+if app.debug:
+	print " * Running in debug mode"
+	import mockdb
+	col = mockdb.MockDb()
+	def get_collection():
+		return col
+else:
+	app.config['MONGO_DBNAME'] = db_name_from_uri(app.config['MONGO_URI'])
+	mongo = PyMongo(app)
+	if mongo:
+		print " * Connection to database established"
+		def get_collection():
+			return mongo.db.test
+
+app.jinja_env.filters['format_date'] = string_from_datetime
+app.jinja_env.filters['format_date_short'] = short_string_from_datetime
+
 
 @app.route('/')
-def hello():
-    return 'Hello World! Byebye!'
+def root():
+    return render_template("index.html")
 
-app.run(debug=True)
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port, debug=app.debug)	
+	print "Finished"
