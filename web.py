3a501405194b7d7bff8372ad2dc7a1686b79fa6d
Yoav Ram <yoavram@gmail.com>
2013-10-21 15:41:22 +0300

    debug keywork bug fix

--- web.py
+++ web.py
@@ -284,5 +284,5 @@ def reorder_cursor(cursor, collection):
 
 if __name__ == '__main__':	
 	port = int(os.environ.get('PORT', 5005))
-	app.run(host='0.0.0.0', port=port, app.debug)	
+	app.run(host='0.0.0.0', port=port, debug=app.debug)	
 	print "Finished"
